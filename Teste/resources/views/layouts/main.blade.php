67a997ee91aff6613bf221011a7364b327cf3b6f
Saulo Pereira Vasconcelos <saulovasconcelos07@gmail.com>
2025-02-25 11:54:22 -0300

    Adicionando icones que vai ser usado no projeto

--- Teste/resources/views/layouts/main.blade.php
+++ Teste/resources/views/layouts/main.blade.php
@@ -52,10 +52,15 @@
 
 
         @yield('content')
+
+        <footer>
+        <p>Saulo Events &copy; 2025</p>
+        </footer>
+
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     </body>
 
-    <footer>
-        <p>Saulo Events &copy; 2025</p>
-    </footer>
+    
 
 </html>
